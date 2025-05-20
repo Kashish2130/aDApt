874d50db5418a23832e341320dd323db0d68a6a0
Kashish2130 <jainkashish2130@gmail.com>
2025-05-20 18:22:31 +0530

    solved another merge conflicts

--- client/src/pages/ImpEmailsPage.jsx
+++ client/src/pages/ImpEmailsPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
-import { AuthContext } from "../context/AuthContext";
+import { AuthContext } from "../context/authContext";
 import {
   MailIcon,
   PencilIcon,
